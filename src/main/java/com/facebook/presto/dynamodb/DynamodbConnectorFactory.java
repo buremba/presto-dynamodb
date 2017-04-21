795fdfcb4ddf8460a6de03f982896e1b5781a02d
Burak Emre Kabakcı <emrekabakci@gmail.com>
2017-04-21 13:02:32 +0300

    Update DynamodbConnectorFactory.java

--- src/main/java/com/facebook/presto/dynamodb/DynamodbConnectorFactory.java
+++ src/main/java/com/facebook/presto/dynamodb/DynamodbConnectorFactory.java
@@ -16,7 +16,6 @@ package com.facebook.presto.dynamodb;
 import com.facebook.presto.spi.ConnectorHandleResolver;
 import com.facebook.presto.spi.connector.Connector;
 import com.facebook.presto.spi.connector.ConnectorFactory;
-import com.facebook.presto.spi.type.TypeManager;
 import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableMap;
 import com.google.inject.Injector;
